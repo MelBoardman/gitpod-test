f50eb8b85b9a84ddd5004b80842348db7d5c32ff
MelBoardman <melanie_boardman@hotmail.com>
2020-01-11 19:50:11 +0000

    Initial Commit - making sure it works

---
+++ jasmine-testing/scripts/calc.js
@@ -0,0 +1,14 @@
+Calculator = function() {
+    this.value = 0;
+};
+
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {
+        this.value += number;
+    } else {
+        alert("Error!");
+    }
+};
+
+
+    
